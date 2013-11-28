e95a9ff3c3327029ff0e57017a9a0a7ac1650e1c
wikitopian <matt@swarmstrategies.com>
2013-11-28 09:58:40 -0500

    edit readme, create class

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-btc-tip-jar
-===========
+Bitcoin Tip Jar
+===============
 
 Easily tip specific posts with bitcoins.
+
+
+Project Author
+---------------
+
+[@wikitopian](https://github.com/wikitopian)
+
+Donations
+---------
+
+* PayPal: `parrott.matt@gmail.com`
+
+* Bitcoin: `13fEAS4B9dTMNXS64Zyq58wCvp4yqnTp6s`
+
+License
+-------
+
+LGPLv3
---
+++ btc-tip-jar.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Plugin Name: Bitcoin Tip Jar
+ * Plugin URI:  https://bitbucket.org/wikitopian/btc-tip-jar
+ * Description: Easily tip specific posts with bitcoins.
+ * Version:     0.1
+ * Author:      @wikitopian
+ * Author URI:  http://www.swarmstrategies.com/matt
+ * License:     LGPLv3
+ * */
+
+class Btc_Tip_Jar {
+}
+$btc_tip_jar = new Btc_Tip_Jar();
